1f1acb7b83a4f0d38e58e039d4e991c5cfac8699
dashIink <71524804+dashIink@users.noreply.github.com>
2023-02-09 08:51:11 -0500

    Added functionality to players.c

--- players.c
+++ players.c
@@ -13,12 +13,22 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+    for (int i = 0; i < num_players, i++){
+    	if (players[i].name == name){
+    		return true;
+    	}
+    }
     return false;
+    
 }
 
 // Go through the list of players and update the score for the 
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for (int i = 0; i < num_players, i++){
+    	if (players[i].name == name){
+    		players[i].score = score;
+    	}
+    }
 }
